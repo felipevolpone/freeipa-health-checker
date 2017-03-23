a05e611c4233e42338d8e1f557329600c5149c95
felipe <fbarreto@localhost.localdomain>
2017-03-23 12:12:32 -0300

    Creating the basic foundation of the CLI. Creating a simple CLI to list certificates found in a path.

--- ipa_health_checker/checker.py
+++ ipa_health_checker/checker.py
@@ -1,15 +1,70 @@
 
+import argparse
 import sys
+import subprocess
+from subprocess import Popen
+import logging
 
-# adding freeipa to pythonpath
-# sys.path.append(os.path.join(os.path.dirname(__file__), "freeipa"))
-# import ipapython
+
+def create_logger():
+    root = logging.getLogger('ipa-hc')
+    root.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s\
+     - %(message)s')
+    console_handler.setFormatter(formatter)
+
+    root.addHandler(console_handler)
 
 
-def parse_args(args):
-    return "checking"
+def get_logger():
+    return logging.getLogger('ipa-hc')
+
+
+class Runner(object):
+
+    def _run(self, command):
+        processor = Popen(command, stderr=None, stdout=subprocess.PIPE,
+                          shell=True)
+        return processor.communicate()
+
+
+class HealthChecker(Runner):
+
+    def __init__(self):
+        self.logger = get_logger()
+
+        parser = argparse.ArgumentParser(description="IPA Health Checker")
+        subparsers = parser.add_subparsers(dest='command')
+
+        list_nssdb = subparsers.add_parser('list_nssdb_certs')
+
+        command = sys.argv[1]
+        if not hasattr(self, command):
+            self.logger.error('command not found')
+            parser.print_help()
+            return
+
+        getattr(self, command)(list_nssdb)
+
+    def list_nssdb_certs(self, subparser):
+        subparser.add_argument('path')
+        subparser.add_argument('--cert_name', help='certifacate name')
+        args = subparser.parse_args(sys.argv[2:])
+
+        command = 'certutil -d {} -L'
+        command = command.format(args.path)
+
+        if args.cert_name:
+            command += ' -n "{}"'.format(args.cert_name)
+
+        self.logger.debug('Running command {}'.format(command))
+        output = self._run(command)
+        self.logger.info(output)
 
 
 if __name__ == '__main__':
-    parse_args(sys.argv[1:])
-
+    create_logger()
+    HealthChecker()
